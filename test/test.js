eb4c41e570a73a26fad27559241a974431c5dd3f
Nick Delitski <ndelitski@gmail.com>
2016-02-10 02:17:05 +0300

    cleanup tests

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     path = require('path'),
     iget = require('../lib/index')({
         project: 'iget-tests',
-        backend: 'http://localhost:3000'
+        host: 'http://localhost:3000'
     }),
     through = require('through2'),
     expect = require('chai').expect;
@@ -65,34 +65,5 @@ describe('gulp-iget-static', function () {
         });
     });
 
-    //describe('iget regex', function () {
-    //    var extract = require('../lib/extract')({defaultLocale: 'ru'}),
-    //        str;
-    //
-    //    str = extract('sd \niget.en(\'"winter \\\'""is comming"\')\n sdfsd')[0];
-    //    expect(str).to.equal('en_"winter \'""is comming"');
-    //
-    //    str = extract("sd \n iget('отредактировал подзадачу %s', event.childTask.name)('sdfsd')(href='http://google.com')\n sdfs;\ndfsd")[0];
-    //    expect(str).to.equal('ru_отредактировал подзадачу %s');
-    //
-    //});
 });
-//
-//describe('iget regex', function () {
-//    var extract = require('../lib/extract')({defaultLocale: 'ru'});
-//
-//    it('should correctly extract string between quotes', function() {
-//        var str;
-//
-//        str = extract('sd \niget.en(\'"winter \\\'""is comming"\')\n sdfsd')[0];
-//        expect(str).to.equal('en_"winter \'""is comming"');
-//
-//        str = extract("sd \n iget('отредактировал подзадачу %s', event.childTask.name)('sdfsd')(href='http://google.com')\n sdfs;\ndfsd")[0];
-//        expect(str).to.equal('ru_отредактировал подзадачу %s');
-//    });
-//
-//
-//    it('should ignore iget\'s without quotes', function () {
-//        str = extract("await iget(this.options.iget)")[0];
-//    });
-//});
+
